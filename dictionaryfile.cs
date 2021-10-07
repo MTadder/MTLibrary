ef191727d4d941f935c41e18bafb6bdbe14e4788
MTadder <MTadder@protonmail.com>
2021-10-07 17:14:44 -0400

    Modernized DictionaryFile & added regions

--- dictionaryfile.cs
+++ dictionaryfile.cs
@@ -4,19 +4,69 @@ using System.Collections.Generic;
 
 namespace MTLibrary {
     public class DictionaryFile {
+        #region Constructors
+        public DictionaryFile(String path) {
+            this._memory = new();
+            this.targetInfo = new(path);
+            this.Load();
+        }
+        #endregion
+
+        #region Properties
         private Dictionary<String, String> _memory;
         private Boolean _synced = false;
         private FileInfo targetInfo;
+        public String this[String key] {
+            get {
+                return this.Get(key);
+            }
+            set {
+                if (value is null) {
+                    this.Remove(key);
+                } else {
+                    this.Set(key, value);
+                }
+            }
+        }
+        public String this[Int32 index] {
+            get {
+                var memExplorer = this._memory.GetEnumerator();
+                Int32 i = 0;
+                while (memExplorer.MoveNext() && index <= i) {
+                    if (index.Equals(i)) {
+                        return memExplorer.Current.Value;
+                    }
+                }
+                throw new IndexOutOfRangeException(
+                    $"index {index} is not within memory!");
+            }
+            set {
+                var memExplorer = this._memory.GetEnumerator();
+                Int32 i = 0;
+                while (memExplorer.MoveNext() && index <= i) {
+                    if (index.Equals(i)) {
+                        this[memExplorer.Current.Key] = value;
+                        return;
+                    }
+                }
+                throw new IndexOutOfRangeException(
+                    $"index {index} is not within memory!");
+            }
+        }
+        #endregion
 
+        #region Filesystem
         public void Save() {
             if (this._memory.Count is 0) {
                 try {
                     this.targetInfo.Delete();
                     this.targetInfo.Create().Dispose();
+                } catch {
+                    throw;
+                } finally {
                     this.targetInfo.Refresh();
                     this._synced = this.targetInfo.Exists;
-                    return;
-                } catch { throw; };
+                };
             }
             if (this._synced is false) {
                 using (FileStream targetStream = this.targetInfo.Open(FileMode.Truncate, FileAccess.Write)) {
@@ -43,25 +93,28 @@ namespace MTLibrary {
                 _ = targetStream.Read(targetData);
                 using (MemoryStream memStream = new(targetData)) {
                     using (BinaryReader binReader = new(memStream)) {
-                        Int32 pairs = binReader.ReadInt32();
-                        Boolean flag = this._memory.Count.Equals(pairs) || this._memory.Count.Equals(0);
-                        for (Int32 i=0; i < pairs; i++) {
+                        Int32 pairsToRead = binReader.ReadInt32();
+                        for (Int32 i=0; i < pairsToRead; i++) {
                             this._memory[binReader.ReadString()] = binReader.ReadString();
                         }
-                        this._synced = this._memory.Count.Equals(pairs) && flag;
+                        this._synced = this._memory.Count.Equals(pairsToRead);
                     }
                 }
             }
         }
+        #endregion
 
+        #region Memory
         public void Clear() {
             this._memory.Clear();
             this._synced = false;
         }
-        public void Set(String key, String value) =>
+        public void Set(String key, String value) {
             (this._memory[key], this._synced) = (value, false);
-        public void Set(String key) => this.Set(key, String.Empty);
-
+        }
+        public void Set(String key) {
+            this.Set(key, String.Empty);
+        }
         public void Remove(String key) {
             _ = this._memory.Remove(key);
             this._synced = false;
@@ -72,7 +125,7 @@ namespace MTLibrary {
                 return true;
             } catch { return false; }
         }
-        public Boolean IsValue(String value) {
+        public Boolean Contains(String value) {
             var explorer = this._memory.GetEnumerator();
             while (explorer.MoveNext()) {
                 if (explorer.Current.Value.Equals(value))
@@ -80,14 +133,9 @@ namespace MTLibrary {
             } return false;
         }
         public String Get(String key) {
-            try { return this._memory[key];
-            } catch (KeyNotFoundException) { return String.Empty; }
+            try { return this._memory[key]; }
+            catch (KeyNotFoundException) { return String.Empty; }
         }
-
-        public DictionaryFile(String path, Boolean isLocal = false) {
-            (this._memory, this.targetInfo) = (new(),
-                new(isLocal ? Environment.CurrentDirectory +@"\"+ path : path));
-            this.Load();
-        }
+        #endregion
     }
 }
