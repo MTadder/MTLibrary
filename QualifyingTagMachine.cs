9b821f04e062e931ead124ce0ce3a02615ae0ea5
MTadder <MTadder@protonmail.com>
2022-06-13 00:46:01 -0400

    DF Adjustments

---
+++ QualifyingTagMachine.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTLibrary {
+    public class QualifyingTagMachine {
+        public class Tag {
+            String _match = String.Empty;
+            Object? _rtn = null;
+            public Tag(String match, Object rtn) {
+                (this._match, this._rtn) = (match, rtn);
+            }
+            public Object Evaluate(String query) {
+                return query.Contains(this._match) ? this._rtn ?? false : false;
+            }
+        }
+        public class Qualifier {
+            public Qualifier(Object key, Tag tag) {
+
+            }
+        }
+        public QualifyingTagMachine() {
+
+        }
+    }
+}
--- dictionaryfile.cs
+++ dictionaryfile.cs
@@ -5,12 +5,14 @@ using System.IO;
 namespace MTLibrary {
     public class DictionaryFile {
         #region Internals
-        internal static readonly String extension = ".bin";
-        internal static String GetAnonymousFilename(Int32 iteration = 0) {
-            if (iteration >= Int32.MaxValue) { return new Random().Next().ToString(); }
-            String gotAnonName = Guid.NewGuid().ToString() + extension;
-            gotAnonName = gotAnonName.Replace("-", "");
-            return File.Exists(gotAnonName) ? GetAnonymousFilename(iteration + 1) : gotAnonName;
+        internal static Boolean HasExtension(String inStr, String ext) {
+            return String.IsNullOrEmpty(inStr) is not true && inStr.Contains($".{ext}");
+        }
+        internal static String GetAnonymousFilename(String ext = "bin", Int32 iteration = 0) {
+            if (iteration >= Int32.MaxValue) { throw new OverflowException(); }
+            String name = $"{Guid.NewGuid()}.{ext}";
+            name = name.Replace('-', '0');
+            return File.Exists(name) ? GetAnonymousFilename(ext, iteration+1) : name;
         }
         #endregion
 
@@ -24,6 +26,11 @@ namespace MTLibrary {
             this._memory = new(origin._memory);
             this._targetInfo = origin._targetInfo;
         }
+        public DictionaryFile(DictionaryFile origin, String name) {
+            this._memory = new(origin._memory);
+            this._targetInfo = new(name);
+            this.Load();
+        }
         public DictionaryFile(String name) {
             this._memory = new();
             this._targetInfo = new(name);
@@ -33,36 +40,52 @@ namespace MTLibrary {
 
         #region Properties
         private Dictionary<String, String> _memory;
-        private Boolean _synced = false;
+        private Boolean _inSync = false;
         private FileInfo _targetInfo;
         public Int32 Count { get { return this._memory.Count; } }
-        public String FullName { get { return this._targetInfo.FullName; } }
-        public String Name { get { return this._targetInfo.Name; } }
+        public Boolean IsSynchronized { get { return this._inSync; } }
+        public String FileName {
+            get { this._targetInfo.Refresh(); return this._targetInfo.Name; }
+            set { 
+                if (value is null) {
+                    _ = this.Delete();
+                } else {
+                    try { this._targetInfo.Delete(); } catch { }
+                    this._targetInfo = new(value);
+                    if (this._targetInfo.Exists is false) {
+                        this.Load();
+                    }
+                }
+            }
+        }
+        public String FilePath {
+            get { this._targetInfo.Refresh(); return this._targetInfo.FullName; }
+        }
         public String this[String key] {
             get { return this.Get(key); }
             set {
-                if (value is not null) { this.Set(key, value); } else { this.Remove(key); }
+                if (value is not null) { this.Set(key, value); } else { _ = this.Remove(key); }
             }
         }
         public static implicit operator Dictionary<String, String>(DictionaryFile df) {
             return new(df._memory);
         }
         public static explicit operator DictionaryFile(Dictionary<String, String> pairs) {
-            var df = new DictionaryFile();
+            DictionaryFile df = new();
             df._memory = pairs;
             return df;
         }
         public static explicit operator DictionaryFile(String[] pairs) {
-            var df = new DictionaryFile();
             if (pairs.Length % 2 == 0) {
+                DictionaryFile df = new();
                 for (Int32 i = 0; i < pairs.Length; i += 2) {
                     try {
                         String key = pairs[i];
                         try {
-                            String val = pairs[i + 1];
+                            String val = pairs[i+1];
                             df.Set(key, val);
-                        } catch { continue; }
-                    } catch { continue; }
+                        } catch { throw; }
+                    } catch { throw; }
                 }
                 return df;
             } else {
@@ -77,7 +100,7 @@ namespace MTLibrary {
         }
         public static DictionaryFile operator -(DictionaryFile df1, DictionaryFile df2) {
             var explorer = ((Dictionary<String, String>) df2).GetEnumerator();
-            while (explorer.MoveNext()) { df1.Remove(explorer.Current.Key); }
+            while (explorer.MoveNext()) { _ = df1.Remove(explorer.Current.Key); }
             return df1;
         }
         #endregion
@@ -85,12 +108,11 @@ namespace MTLibrary {
         #region Methods
         public void Save() {
             if (this.Count is 0) {
-                this.Delete();
-                //this._targetInfo.Refresh();
-                this._synced = true;
+                _ = this.Delete();
+                this._inSync = true;
                 return;
             }
-            if (this._synced is false) {
+            if (this._inSync is false) {
                 using (FileStream targetStream = this._targetInfo.Open(FileMode.Truncate, FileAccess.Write)) {
                     using (BinaryWriter binWriter = new(targetStream)) {
                         binWriter.Write(this.Count);
@@ -101,15 +123,21 @@ namespace MTLibrary {
                         }
                     }
                 }
-                this._synced = true;
+                this._inSync = true;
             }
         }
+        /// <summary>
+        /// Opens/Creates the <see cref="DictionaryFile">DictionaryFile</see> on Disk,
+        /// reads it's contents and emplaces the read data pairs into a <see cref="Dictionary{TKey, TValue}">
+        /// Dictionary</see> (discarding non-valid pairs).
+        /// </summary>
+        /// <returns><see cref="void"/></returns>
         public void Load() {
             this._targetInfo.Refresh();
             using (FileStream targetStream = this._targetInfo.Open(FileMode.OpenOrCreate, FileAccess.Read)) {
                 Int32 len = (Int32) targetStream.Length;
                 if (len < 13) {
-                    this._synced = this.Count.Equals(0);
+                    this._inSync = this.Count.Equals(0);
                     return;
                 }
                 Byte[] targetData = new Byte[len];
@@ -126,30 +154,37 @@ namespace MTLibrary {
                                 } catch { continue; }
                             } catch { continue; }
                         }
-                        this._synced = this.Count.Equals(pairsToRead);
+                        this._inSync = this.Count.Equals(pairsToRead);
                     }
                 }
             }
         }
         public void Clear() {
             this._memory.Clear();
-            this._synced = true;
-            this.Delete();
+            this._inSync = false;
         }
         public void Set(String key, String value) {
-            (this._memory[key], this._synced) = (value, false);
+            (this._memory[key], this._inSync) = (value, false);
         }
         public void Set(String key) {
             this.Set(key, String.Empty);
         }
-        public void Remove(String key) {
-            _ = this._memory.Remove(key);
-            this._synced = false;
+        public Boolean Remove(String key) {
+            Boolean rem = this._memory.Remove(key);
+            this._inSync = rem;
+            return rem;
         }
-        public void Delete() {
+        public Boolean Delete() {
             this._targetInfo.Refresh();
-            if (this._targetInfo.Exists)
-                try { this._targetInfo.Delete(); } catch { throw; };
+            if (this._targetInfo.Exists) {
+                try { this._targetInfo.Delete(); } catch { return false; };
+            }
+            this._targetInfo.Refresh();
+            return this._targetInfo.Exists is false;
+        }
+        public void Destroy() {
+            this.Clear();
+            _ = this.Delete();
         }
         public Boolean IsKey(String key) {
             try {
@@ -171,7 +206,7 @@ namespace MTLibrary {
             return false;
         }
         public String Get(String key) {
-            try { return this._memory[key]; } catch (KeyNotFoundException) { return String.Empty; }
+            try { return this._memory[key]; } catch { return String.Empty; }
         }
         #endregion
     }
